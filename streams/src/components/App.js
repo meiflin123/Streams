f73bda506d14689dd74ac7af5595bec1e11c80ef
Meifang Lin <meiflin123@gmail.com>
2019-02-22 17:44:50 -0800

    create sign in and sign out auth through google api

--- streams/src/components/App.js
+++ streams/src/components/App.js
@@ -5,21 +5,25 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
+import Header from './Header';
+
+
 
 
 const App = () => {
 	return (
-    <div>
-    <BrowserRouter>
-      <div>
-        <Route path="/" exact component={ StreamList }/>
-        <Route path="/stream/new" exact component={ StreamCreate }/>
-        <Route path="/stream/delete" exact component={ StreamDelete }/>
-        <Route path="/stream/edit" exact component={ StreamEdit }/>
-        <Route path="/stream/show" exact component={ StreamShow }/>
-  
-      </div>
-    </BrowserRouter>
+    <div className="ui container">
+
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route path="/" exact component={ StreamList }/>
+          <Route path="/stream/new" exact component={ StreamCreate }/>
+          <Route path="/stream/delete" exact component={ StreamDelete }/>
+          <Route path="/stream/edit" exact component={ StreamEdit }/>
+          <Route path="/stream/show" exact component={ StreamShow }/>
+        </div>
+      </BrowserRouter>
     </div>
   );
 };
