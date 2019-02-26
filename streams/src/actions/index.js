a4ab14c9c516bd247809f792342b8d3e4b9b043b
Meifang Lin <meiflin123@gmail.com>
2019-02-25 16:44:32 -0800

    create streamReducer

--- streams/src/actions/index.js
+++ streams/src/actions/index.js
@@ -43,6 +43,7 @@ export const editStream = (id, formValues) => async dispatch => {
 };
 
 export const deleteStream = id => async dispatch => {
+  // eslint-disable-next-line
   const response = await streams.delete(`/streams/${id}`);
-  dispatch({ type: DELETE_STREAM, payload: response.data })
+  dispatch({ type: DELETE_STREAM, payload: id })
 };
