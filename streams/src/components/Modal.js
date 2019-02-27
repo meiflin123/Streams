cfc7e67023bdbd9cd8ae6f70f46b74c59a9d405f
Meifang Lin <meiflin123@gmail.com>
2019-02-27 14:29:22 -0800

    refractor modal to be useable

--- streams/src/components/Modal.js
+++ streams/src/components/Modal.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import history from '../history';
 
 const Modal = props => {
   // navigate to '/' path if any grey area is clicked.
   // handle event progagation.
   
   return ReactDOM.createPortal(
-    <div onClick={ () => history.push('/') } className="ui dimmer modals visibale active">
+    <div onClick={ props.onDismiss } className="ui dimmer modals visibale active">
       <div onClick={ e => e.stopPropagation() }className="ui standard modal visibale active">
-        <div className="header">Delete Stream</div>
+        <div className="header">{ props.header}</div>
         <div className="content">
-          <p>Are you sure you want to delete this stream?</p>
+          <p>{ props.content }</p>
         </div>
         <div className="actions">
-          <div className="ui negative button">DELETE</div>
-          <div className="ui button">Cancel</div>
-
+          { props.actions }
         </div>
       </div>
     </div>,
