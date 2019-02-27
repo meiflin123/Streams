93459c0604013ae2a0897ffe8ea6ff157db0d603
Meifang Lin <meiflin123@gmail.com>
2019-02-26 18:02:02 -0800

    create reusable StreamForm and refractor StreamCreate and StreamEdit

--- streams/src/components/streams/StreamCreate.js
+++ streams/src/components/streams/StreamCreate.js
@@ -1,79 +1,23 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form'; 
 import { connect } from 'react-redux';
 import { createStream } from '../../actions/index.js';
+import StreamForm from './StreamForm';
 
 class StreamCreate extends React.Component {
 
-  // when user has touched the form and error, display error.
-  renderError({ error, touched }) {
-    if(touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-
-  }
-  
-
-  renderInput = ({ input, label, meta }) => {
-    const className = `field  ${meta.error && meta.touched? 'error' : ''}`
-    console.log(input, label, meta.touched)
-
-    // add input from formProps as property to input element (cuz need value and onChange)
-    // display error if any
-
-    return (
-      <div className={className}>
-        <label>{ label }</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  }
 
   onSubmit = formProps => {
-    console.log(formProps);
     this.props.createStream(formProps)
   }
   
   render() {
-    //use handleSubmit from redux-form to handle event
-    //disable semantic ui's default feature of hiding error
-    //pass label to renderInput
-
     return (
-       <form onSubmit={this.props.handleSubmit(this.onSubmit)} className="ui form error">
-         <Field name="title" component={this.renderInput} label="Enter Title"/>
-         <Field name="description" component={this.renderInput} label="Enter Description"/>
-         <button className="ui button primary">Submit</button>
-
-       </form>
+      <div>
+        <h3>Create Stream</h3>
+        <StreamForm onSubmit={this.onSubmit}/>
+      </div>
     );
   }
 }
 
-// define validate function outside component
-  // empty object --> no error
-  // object --> error, reduxform matches Field name and pass error to this.renderInput.
-
-const validate = formProps => {
-  const errors = {}
-  if (!formProps.title) {
-    errors.title = "You must enter a title";
-  }
-  if (!formProps.description) {
-    errors.description = "You must enter a description";
-  }
-  return errors;
-}
-
-// connect redux form with StreamCreate
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate
-})(StreamCreate);
-
-export default connect(null, { createStream })(formWrapped)
+export default connect(null, { createStream })(StreamCreate)
--- streams/src/components/streams/StreamEdit.js
+++ streams/src/components/streams/StreamEdit.js
@@ -1,8 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStreams, editStream } from '../../actions/index.js';
+import StreamForm from './StreamForm';
+import _ from 'lodash';
 
-const StreamEdit = () => {
-  return <div>StreamEdit</div>;
+class StreamEdit extends React.Component {
+
+  componentDidMount() {
+    this.props.fetchStreams()
+  };
+
+  onSubmit = (formProps)=> {
+    //console.log(formProps)
+    this.props.editStream(this.props.match.params.id, formProps)
+    
+  }
+  
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading</div>
+    };
+
+    return (
+      <div>
+        <h3>Edit Stream</h3>
+        <StreamForm initialValues={_.pick(this.props.stream, 'title', 'description')} onSubmit={this.onSubmit}/>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  console.log(state.streams[ownProps.match.params.id])
+  return {
+    stream: state.streams[ownProps.match.params.id]
+  };
 };
 
-
-export default StreamEdit;
+export default connect(mapStateToProps, { editStream, fetchStreams })(StreamEdit)
